e019ae81c34f533869204ecc5a9af71740fdbe08
harbouli <mharbouli@icloud.com>
2022-09-21 20:38:33 +0100

    Set Up Routers

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import AuthenticationPage from './pages/AuthenticationPage';
+
+function App ()
+{
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={ <AuthenticationPage /> }></Route>
+        <Route
+          path="conversations"
+          element={
+            <div>
+              <div>Conversations</div>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route path=":id" element={ <div>Conversation ID Page</div> } />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/AuthenticationPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function AuthenticationPage ()
+{
+    return (
+        <div>AuthenticationPage</div>
+    )
+}
+
+export default AuthenticationPage
